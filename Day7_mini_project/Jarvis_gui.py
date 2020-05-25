7510fa3ac0b2c6fd3ca9bf464885027f7f2c1d05
Divyanshu Jain <55808340+thevyanshu@users.noreply.github.com>
2020-05-25 09:15:06 +0530

    Update Jarvis_gui.py

--- Day7_mini_project/Jarvis_gui.py
+++ Day7_mini_project/Jarvis_gui.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Requires PyAudio and PySpeech.
+
 
 import webbrowser as wb
 import speech_recognition as sr
@@ -43,19 +43,7 @@ def recordAudio():
         print("heard...")
     # Speech recognition using Google Speech Recognition
     data = ""
- #    try:
- #    # for testing purposes, we're just using the default API key
- #    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
- #    # instead of `r.recognize_google(audio)`
- #    data = r.recognize_google(audio))
-	#     print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-	# except sr.UnknownValueError:
- #    	print("Google Speech Recognition could not understand audio")
-	# except sr.RequestError as e:
- #    	print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-
-
+ 
     try:
     # Uses the default API key
     # To use another API key: `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
